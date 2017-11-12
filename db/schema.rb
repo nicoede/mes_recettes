84151471dd81c4c4e9da1d63ec378aaaa775a5fa
Edenilson J dos Passos <edenilson.passos@yahoo.com>
2017-11-12 06:19:48 +0000

    recipes and chefs TDD

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112025134) do
+ActiveRecord::Schema.define(version: 20171112055927) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171112025134) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chef_id"
   end
 
   create_table "users", force: :cascade do |t|
