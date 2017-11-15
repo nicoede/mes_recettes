53afe28d456002d64ad7bb4b8f72902a49072334
Edenilson J dos Passos <edenilson.passos@yahoo.com>
2017-11-15 05:14:30 +0000

    Added pagination

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114051011) do
+ActiveRecord::Schema.define(version: 20171115042935) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
     t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.boolean  "like"
+    t.integer  "chef_id"
+    t.integer  "recipe_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -28,9 +36,6 @@ ActiveRecord::Schema.define(version: 20171114051011) do
     t.datetime "updated_at"
     t.integer  "chef_id"
     t.string   "picture"
-  end
-
-  create_table "users", force: :cascade do |t|
   end
 
 end
