bb1e53394311ea833cb3481107d20e75c06ae258
Edenilson J dos Passos <edenilson.passos@yahoo.com>
2017-11-16 07:17:00 +0000

    authentication system and chef pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,7 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:edit, :update, :show, :like]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update]
   
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
@@ -14,7 +17,7 @@ class RecipesController < ApplicationController
   
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(3)
+    @recipe.chef = current_user
     
     if @recipe.save
   	  flash[:success] = "Your recipe was created successfully!"
@@ -25,11 +28,10 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    @recipe = Recipe.find(params[:id])
+    
   end
   
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
     	flash[:success] = "Your recipe was updated successfully!"
     	redirect_to recipe_path(@recipe)
@@ -40,8 +42,7 @@ class RecipesController < ApplicationController
   end
   
   def like
-    @recipe = Recipe.find(params[:id])
-    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
     if like.valid?
       flash[:success] = "Your selection was successful"
       redirect_to :back
@@ -52,8 +53,18 @@ class RecipesController < ApplicationController
   end
   
   private
-      
       def recipe_params
         params.require(:recipe).permit(:name, :summary, :description, :picture)
+      end
+  
+      def set_recipe
+        @recipe = Recipe.find(params[:id])
+      end
+      
+      def require_same_user
+        if current_user != @recipe.chef
+          flash[:danger] = "You can only edit your own recipe!"
+          redirect_to recipes_path
+        end
       end
 end
