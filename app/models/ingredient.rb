5ff7a2748f00fd54c7dccb6297f4c504af4c7f90
Edenilson J dos Passos <edenilson.passos@yahoo.com>
2017-11-18 03:53:16 +0000

    Ingredients and style

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ActiveRecord::Base
-  validates :name, presence: true, length: { minimum: 5, maximum: 25 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 35 }
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,5 +1,5 @@
 class Style < ActiveRecord::Base
-  validates :name, presence: true, length: { minimum: 5, maximum: 25 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 35 }
   has_many :recipe_styles
   has_many :recipes, through: :recipe_styles
 end
